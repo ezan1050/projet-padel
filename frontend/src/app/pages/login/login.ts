e64225a39776412863dcd6f4e9b89c8bc2098904
ezan bhatti <ezan1050@gmail.com>
2026-07-15 12:34:37 +0200

    Frontend : redirection automatique vers les matchs apres connexion

--- frontend/src/app/pages/login/login.ts
+++ frontend/src/app/pages/login/login.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 import { Auth } from '../../services/auth';
 
 @Component({
@@ -14,14 +15,17 @@ export class Login {
   motDePasse = '';
   message = '';
 
-  constructor(private authService: Auth) { }
+  constructor(
+    private authService: Auth,
+    private router: Router
+  ) { }
 
   seConnecter(): void {
     this.authService.login(this.matricule, this.motDePasse).subscribe({
       next: (reponse) => {
         localStorage.setItem('token', reponse.token);
         localStorage.setItem('role', reponse.role);
-        this.message = 'Connexion reussie ! Role : ' + reponse.role;
+        this.router.navigate(['/matchs']);
       },
       error: () => {
         this.message = 'Matricule ou mot de passe incorrect';
